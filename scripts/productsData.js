0f8c5b6cfe0f4e48cc822e94f7fa5af39dfd5bae
Safei Ashraf <safei.ashraf@gmail.com>
2023-03-03 09:37:57 +0200

    feat: create data in separate file

---
+++ scripts/productsData.js
@@ -0,0 +1,49 @@
+export const products = [
+	{
+		name: "Careflection M10 in-Ear TWS Wireless Touch Bluetooth Earplugs",
+		id: "product_1",
+		price: "279.00",
+		imageUrl:
+			"https://m.media-amazon.com/images/I/71MgxH82r7L._AC_SL1500_.jpg",
+		isAddedToCart: false,
+	},
+	{
+		name: "Airpods PRO 2Sime original | AirpodsPro 2 Semi Original",
+		id: "product_2",
+		price: "800.00",
+		imageUrl:
+			"https://m.media-amazon.com/images/I/51V8CNfknlL._AC_SL1500_.jpg",
+		isAddedToCart: false,
+	},
+	{
+		name: "AirPods Sports Waterproof LED Display with Internal Powerbank Plus Digital Indicator - V5.1/M10",
+		id: "product_3",
+		price: "449.00",
+		imageUrl:
+			"https://m.media-amazon.com/images/I/614-+ZjwojL._AC_SL1000_.jpg",
+		isAddedToCart: false,
+	},
+	{
+		name: "Anker Soundcore Life Note E, Black, True Wireless Earbuds with Big Bass and 3 EQ Modes",
+		id: "product_4",
+		price: "1,950.00",
+		imageUrl:
+			"https://m.media-amazon.com/images/I/61-uda1MkpL._AC_SL1500_.jpg",
+		isAddedToCart: false,
+	},
+	{
+		name: "Xiaomi Redmi Buds 3 Lite Wireless Headphones Approx. 5h Earphone Battery Life 10 meters Bluetooth RangeBlack, Black, 4.3 g",
+		id: "product_5",
+		price: "719.00",
+		imageUrl:
+			"https://m.media-amazon.com/images/I/51HhYD53fML._AC_SL1500_.jpg",
+		isAddedToCart: false,
+	},
+	{
+		name: "MI Redmi AirDots Pro Bluetooth 5.0 Wireless With Digital LED Earbuds - Black",
+		id: "product_6",
+		price: "250.00",
+		imageUrl: "https://m.media-amazon.com/images/I/31qsDSdUAeL._AC_.jpg",
+		isAddedToCart: false,
+	},
+];
