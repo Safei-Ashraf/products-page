ad9c2bfbf668c1bccba43c379946c5837934b67e
Safei Ashraf <safei.ashraf@gmail.com>
2023-03-05 09:09:06 +0200

    feat: create cart updater

---
+++ scripts/Cart/updateCartCount.js
@@ -0,0 +1,9 @@
+export function updateCartCount(products, cartElem) {
+    let count = 0;
+    products.forEach(product => {
+        if (product.isAddedToCart === true) {
+            count += 1;
+        }
+    })
+    cartElem.textContent = count;
+}
